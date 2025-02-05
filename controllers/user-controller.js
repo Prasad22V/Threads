fc595688e9c59137f8871694314421625ff52362
Prasad22V <prasadvaidya20818@gmail.com>
2025-02-05 13:18:28 +0530

    Replaced bcrypt with bcryptjs

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,5 @@
 const User = require("../models/user-model");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const formidable = require("formidable");
 const cloudinary = require("../config/cloudinary");
